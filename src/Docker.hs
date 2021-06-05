1228f479432e303d21a210979715e2c05bdf995f
FreerGit <fredrik-98@live.se>
2021-06-05 17:58:12 +0200

    feat: parse containerId on image creation

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,6 +1,8 @@
 module Docker where
 
 import RIO
+import Data.Aeson ((.:))
+import qualified Data.Aeson.Types as Aeson.Types
 import qualified Network.HTTP.Simple as HTTP
 import qualified Data.Aeson as Aeson
 import qualified Sockets
@@ -22,22 +24,46 @@ data CreateContainerOptions
     { image :: Image
     }
 
-createContainer :: CreateContainerOptions -> IO ()
+newtype ContainerId = ContainerId Text
+  deriving (Eq, Show)
+
+containerIdToText :: ContainerId -> Text
+containerIdToText (ContainerId id) = id
+
+createContainer :: CreateContainerOptions -> IO ContainerId
 createContainer options = do
     manager <- Sockets.newManager "/var/run/docker.sock"
     let image = imageToText options.image
     let body = Aeson.object 
-                [ ("Image", Aeson.toJSON image),
-                  ("Tty", Aeson.toJSON True),
-                  ("Labels", Aeson.object [("HASCI", "")]),
-                  ("Cmd", "echo hello world"),
-                  ("Entrypoint", Aeson.toJSON [Aeson.String "/bin/sh", "-c"])
-                ]
+            [ ("Image", Aeson.toJSON image),
+              ("Tty", Aeson.toJSON True),
+              ("Labels", Aeson.object [("HASCI", "")]),
+              ("Cmd", "echo hello world"),
+              ("Entrypoint", Aeson.toJSON [Aeson.String "/bin/sh", "-c"])
+            ]
+
     let req = HTTP.defaultRequest
             & HTTP.setRequestManager manager
             & HTTP.setRequestPath "/v1.41/containers/create"
             & HTTP.setRequestMethod "POST"
             & HTTP.setRequestBodyJSON body
+
+    let parser = Aeson.withObject "create-container" $ \obj -> do
+        cId <- obj .: "Id"
+        pure $ ContainerId cId
+    
     res <- HTTP.httpBS req
-    traceShowIO res
-            
+    parseResponse res parser
+
+parseResponse
+  :: HTTP.Response ByteString 
+  -> (Aeson.Value -> Aeson.Types.Parser a)
+  -> IO a
+parseResponse res parser = do
+  let result = do
+      value <- Aeson.eitherDecodeStrict (HTTP.getResponseBody res)
+      Aeson.Types.parseEither parser value
+
+  case result of
+    Left e -> throwString e
+    Right status -> pure status
